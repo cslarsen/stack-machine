9597fd80cd3c2c8b40b10b57e0040985cfbbf60a
Christian Stigen Larsen <csl@sublevel3.org>
2010-01-22 12:21:48 +0100

    Compile from standard input

--- sm-compile.cpp
+++ sm-compile.cpp
@@ -20,6 +20,14 @@ static void compile_error(const char* msg)
   fprintf(stderr, "%s:%d:%s\n", file, p->get_lineno(), msg);
 }
 
+void compile(FILE* f, const std::string& out)
+{
+  delete(p);
+  p = new parser(f);
+  compiler c(*p, compile_error);
+  c.get_program().save_image( fileptr(fopen(out.c_str(), "wb")));
+}
+
 int main(int argc, char** argv)
 {
   try {
@@ -27,15 +35,11 @@ int main(int argc, char** argv)
       error("Usage: sm-compile [ filename(s) ]");
 
     for ( int n=1; n<argc; ++n ) {
-      fprintf(stderr, "Compiling %s\n", argv[1]);
 
-      fileptr f(fopen(argv[1], "rt"));
-      delete(p);
-      p = new parser(f);
-      compiler c(*p, compile_error);
-
-      std::string file = basename(argv[1]) + ".sm";
-      c.get_program().save_image( fileptr(fopen(file.c_str(), "wb")));
+      if ( !strcmp(argv[n], "-") )
+        compile(stdin, "out.sm");
+      else
+        compile(fileptr(fopen(argv[n], "rt")), basename(argv[n]) + ".sm");
     }
 
     return 0;
